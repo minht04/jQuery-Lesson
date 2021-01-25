647fd998cba297908287ffed77f85dd6b9346006
hitomi <sky.force.1013@gmail.com>
2021-01-25 10:55:33 +0900

    Initial Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+$(function(){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'heigth': '100px'
+    }).slideUp();
+  });
+});
